726108803ee071caee1aa8472aaeba1da82d6e28
Kyle Romero <kgromero@gmail.com>
2024-08-07 10:43:43 -0400

    Update Configuration.cs

--- RedditListener/Configuration.cs
+++ RedditListener/Configuration.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,14 +16,22 @@ namespace RedditListener
         public readonly string[] SubRedditsToMonitor =
                 {
                 "askreddit",
-                "gaming"
-                //"funny",
-                //"worldnews",
-                //"todayilearned"
+                "news",
+                "gaming",
+                "funny",
+                "worldnews",
+                "todayilearned"
             };
         //How many posts do I want to show for each subreddit? (Top #)
         public readonly int NumberOfPostsToTrack = 5;
         //How many Authors do I want to show for each subreddit? (Top #)
         public readonly int NumberOfAuthorsToTrack = 5;
+
+        // new = Look at all posts made from the point that the app was started and calculate the top NumberOfPostsToTrack and NumberOfAuthorsToTrack from those posts.
+        //      This uses the new.json listing and will page backwards through the listing data slices until it finds the first post that was created after start.
+        //      Time between requests will slow down over time as the number of pages you have to go through increases, as each page uses a request.
+        // top = Look at the top posts listing and calculate the top NumberOfPostsToTrack and NumberOfAuthorsToTrack from those posts. Looks at current top 100.
+        public string mode = "top";
+
     }
 }
